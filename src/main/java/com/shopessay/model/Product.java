d53b0f6f33d1410d01c477cdaaa6e89ca184955f
umukundwaa <a.umukundwa1@alustudent.com>
2026-03-18 07:20:33 -0700

    Add Product model

---
+++ src/main/java/com/shopessay/model/Product.java
@@ -0,0 +1,21 @@
+package com.shopessay.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Product {
+    private Long id;
+    private String name;
+    private String  description;
+    private Double price;
+    private Integer quantity;
+    private  Boolean available;
+    private  String category;
+
+}
+
