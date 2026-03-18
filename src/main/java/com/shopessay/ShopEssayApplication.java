d2a284809bcdb2812d5d6ca3bc947e2eb85a4afd
umukundwaa <a.umukundwa1@alustudent.com>
2026-03-18 06:53:48 -0700

    Initial Spring Boot setup

---
+++ src/main/java/com/shopessay/ShopEssayApplication.java
@@ -0,0 +1,13 @@
+package com.shopessay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopEssayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopEssayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shopessay/ShopEssayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopessay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopEssayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
